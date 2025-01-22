c1d05d4f26e9df5c3aee55bbc6db07af7c1b13ef
yong <tkddbs1644@gmail.com>
2025-01-23 08:46:35 +0900

    chore: 불필요한 서비스와 패키지 제거

--- apps/web/eslint.config.mjs
+++ apps/web/eslint.config.mjs
@@ -9,6 +9,19 @@ const compat = new FlatCompat({
   baseDirectory: __dirname,
 });
 
-const eslintConfig = [...compat.extends("next/core-web-vitals", "next/typescript")];
+const eslintConfig = [
+  ...compat.extends("next/core-web-vitals", "next/typescript"),
+  {
+    rules: {
+      "no-warning-comments": [
+        "warn",
+        {
+          terms: ["TODO", "FIXME", "XXX", "BUG"],
+          location: "anywhere",
+        },
+      ],
+    },
+  },
+];
 
 export default eslintConfig;
