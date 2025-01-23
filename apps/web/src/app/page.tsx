2f82274cdb800c08a65877565392a6aee60f1c68
yong <tkddbs1644@gmail.com>
2025-01-23 14:30:56 +0900

    config(apps/web): tailwind 설정 추가

--- apps/web/src/app/page.tsx
+++ apps/web/src/app/page.tsx
@@ -5,8 +5,8 @@ import Link from "next/link";
 export default function Welcome() {
   return (
     <Background src="/backgrounds/puzzle.gif">
-      <Image src="/symbol.png" alt="symbol" width={180} height={180} priority />
-      <h1 className="game">퍼즐팝!</h1>
+      <Image src="/symbol.png" alt="symbol" width={240} height={240} priority />
+      <h1 className="game text-5xl">퍼즐팝!</h1>
       <ol>
         <Link href="/singlemode">싱글모드</Link>
       </ol>
