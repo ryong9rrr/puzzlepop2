56b63b038b942971713135bc9d58c353f1665fbc
yong <tkddbs1644@gmail.com>
2025-01-31 18:37:47 +0900

    feat(apps/storybook): FOUNDATION/themes에 orange, lavender, yellow color story 추가

--- apps/storybook/src/stories/Foundation-themes.stories.tsx
+++ apps/storybook/src/stories/Foundation-themes.stories.tsx
@@ -1,41 +1,55 @@
 import type { Meta, StoryObj } from "@storybook/react";
 import "@puzzlepop2/themes/themes.css";
-import { Box } from "@puzzlepop2/react";
+import { Box, Flex } from "@puzzlepop2/react";
+
+const colorLevels = [50, 100, 200, 300, 400, 500, 600, 700, 800, 900];
 
 const BoxStories = () => {
   return (
-    <div style={{ display: "flex" }}>
-      <Box style={{ width: "100px", height: "100px", backgroundColor: "var(--orange-50)" }}>
-        orange50
-      </Box>
-      <Box style={{ width: "100px", height: "100px", backgroundColor: "var(--orange-100)" }}>
-        orange100
-      </Box>
-      <Box style={{ width: "100px", height: "100px", backgroundColor: "var(--orange-200)" }}>
-        orange200
-      </Box>
-      <Box style={{ width: "100px", height: "100px", backgroundColor: "var(--orange-300)" }}>
-        orange300
-      </Box>
-      <Box style={{ width: "100px", height: "100px", backgroundColor: "var(--orange-400)" }}>
-        orange400
-      </Box>
-      <Box style={{ width: "100px", height: "100px", backgroundColor: "var(--orange-500)" }}>
-        orange500
-      </Box>
-      <Box style={{ width: "100px", height: "100px", backgroundColor: "var(--orange-600)" }}>
-        orange600
-      </Box>
-      <Box style={{ width: "100px", height: "100px", backgroundColor: "var(--orange-700)" }}>
-        orange700
-      </Box>
-      <Box style={{ width: "100px", height: "100px", backgroundColor: "var(--orange-800)" }}>
-        orange800
-      </Box>
-      <Box style={{ width: "100px", height: "100px", backgroundColor: "var(--orange-900)" }}>
-        orange900
-      </Box>
-    </div>
+    <>
+      <Flex>
+        {colorLevels.map(level => (
+          <Box
+            key={level}
+            style={{
+              width: "100px",
+              height: "100px",
+              backgroundColor: `var(--orange-${level})`,
+            }}
+          >
+            orange{level}
+          </Box>
+        ))}
+      </Flex>
+      <Flex>
+        {colorLevels.map(level => (
+          <Box
+            key={level}
+            style={{
+              width: "100px",
+              height: "100px",
+              backgroundColor: `var(--lavender-${level})`,
+            }}
+          >
+            lavender{level}
+          </Box>
+        ))}
+      </Flex>
+      <Flex>
+        {colorLevels.map(level => (
+          <Box
+            key={level}
+            style={{
+              width: "100px",
+              height: "100px",
+              backgroundColor: `var(--yellow-${level})`,
+            }}
+          >
+            yellow{level}
+          </Box>
+        ))}
+      </Flex>
+    </>
   );
 };
 
--- packages/react/src/index.ts
+++ packages/react/src/index.ts
@@ -2,4 +2,7 @@
 export { Box } from "./components/Box";
 export type { BoxProps } from "./components/Box";
 
+export { Flex } from "./components/Flex";
+export type { FlexProps } from "./components/Flex";
+
 export { Button } from "./components/Button";
