e1cab08551bbd90a07bbf24d272471c76b0f6d07
yong <tkddbs1644@gmail.com>
2025-02-03 04:59:08 +0900

    chore(packages): core 패키지의 이름을 game으로 변경

--- apps/web/src/app/fetchnote/page.tsx
+++ apps/web/src/app/fetchnote/page.tsx
@@ -1,6 +1,6 @@
 import "@puzzlepop2/themes/themes.css";
 import { Box } from "@puzzlepop2/react-components-layout";
-import { sum } from "@puzzlepop2/core";
+import { sum } from "@puzzlepop2/game";
 
 export default function FetchNotePage() {
   return (
