7a480de432065a5aebfecea64a1436c500a31363
yong <tkddbs1644@gmail.com>
2025-02-07 10:31:47 +0900

    fix(apps/web): http request를 nginx 리버스 프록시로 위임

--- apps/web/src/utils/end-point.ts
+++ apps/web/src/utils/end-point.ts
@@ -1,5 +1,5 @@
 export const getRestServerUrl = () => {
   return process.env.NODE_ENV === "development"
     ? "http://localhost:8080/rest-server"
-    : "/rest-server";
+    : "http://140.245.39.129/rest-server";
 };
