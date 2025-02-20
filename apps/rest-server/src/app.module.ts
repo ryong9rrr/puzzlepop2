51e8853ed819b73e7974774fde5d171980c70cd5
yong <tkddbs1644@gmail.com>
2025-02-21 01:28:12 +0900

    config(apps/rest-server): env.NODE_ENV를 Dockerfile에 추가

--- apps/rest-server/src/app.module.ts
+++ apps/rest-server/src/app.module.ts
@@ -6,11 +6,15 @@ import { AppService } from './app.service';
 import { PuzzlesModule } from './puzzles/puzzles.module';
 import { MongooseModule } from '@nestjs/mongoose';
 
+const MONGO_URI =
+  process.env.NODE_ENV === 'production'
+    ? 'mongodb://puzzlepop2-mongodb:27017/'
+    : 'mongodb://root:1234@localhost:27017/';
+
 @Module({
   imports: [
     ConfigModule.forRoot(),
-    MongooseModule.forRoot('mongodb://puzzlepop2-mongodb:27017/'),
-    //MongooseModule.forRoot('mongodb://root:1234@localhost:27017/'),
+    MongooseModule.forRoot(MONGO_URI),
     PuzzlesModule,
   ],
   controllers: [AppController],
