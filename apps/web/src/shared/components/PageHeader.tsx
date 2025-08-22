97f4907348b8f49977b3c4ec7a84c96e981b03b7
yong <tkddbs1644@gmail.com>
2025-08-22 21:57:43 +0900

    fix(apps/web): PageHeader의 프로그래밍 라우팅을 SafeLink 태그로 교체

--- apps/web/src/shared/components/PageHeader.tsx
+++ apps/web/src/shared/components/PageHeader.tsx
@@ -5,15 +5,14 @@ import Image from "next/image";
 import { Flex } from "@puzzlepop2/react-components-layout";
 import { Z_INDEX } from "@puzzlepop2/themes";
 
-import { useSafeRouter } from "@router/useSafeRouter";
+import { SafeLink } from "@router/SafeLink";
 
 import * as CDN from "@remotes-cdn/images";
 import MODULE_CSS from "./PageHeader.module.css";
 
 export const PageHeader = () => {
-  const router = useSafeRouter();
-  const linkItemTextRef1 = useRef<HTMLDivElement>(null);
-  const linkItemTextRef2 = useRef<HTMLDivElement>(null);
+  const linkItemTextRef1 = useRef<HTMLAnchorElement>(null);
+  const linkItemTextRef2 = useRef<HTMLAnchorElement>(null);
 
   useEffect(() => {
     const handleScroll = () => {
@@ -56,30 +55,25 @@ export const PageHeader = () => {
           }}
         >
           <Flex align="center" gap={20}>
-            <Logo
-              onClick={() => {
-                router.push("/");
-              }}
-            />
+            <SafeLink href="/">
+              <Logo />
+            </SafeLink>
+
             <Flex gap={8} className="font-gameTitle">
-              <div
+              <SafeLink
+                href="/practice/game"
                 ref={linkItemTextRef1}
                 className={MODULE_CSS["link-item-text"]}
-                onClick={() => {
-                  router.push("/practice/game");
-                }}
               >
                 연습모드
-              </div>
-              <div
+              </SafeLink>
+              <SafeLink
+                href="/multi/cooperation"
                 ref={linkItemTextRef2}
                 className={MODULE_CSS["link-item-text"]}
-                onClick={() => {
-                  router.push("/multi/cooperation");
-                }}
               >
                 멀티게임
-              </div>
+              </SafeLink>
             </Flex>
           </Flex>
           <div>{/* 마이페이지 */}</div>
